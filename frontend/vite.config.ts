b6ce4f9540be05f653b5563edfc799fa23f41a46
Vicky Stephens <vicky.k.stephens@gmail.com>
2026-07-04 16:59:08 +0100

    Allow production host in Vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['simonizer.vickystephens.co.uk'],
     host: true,
     port: 5173,
   },
