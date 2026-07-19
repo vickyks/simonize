e333c1e364c8b76e36d38b75a74d84ecc0358424
Vicky Stephens <vicky.k.stephens@gmail.com>
2026-07-19 23:02:06 +0100

    Add frontend styling foundation

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import '@fontsource-variable/source-sans-3'
+import './index.css'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
